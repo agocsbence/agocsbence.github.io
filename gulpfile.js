95f302115bdf10cd1fedc120eff0ba3f95ace1f6
Bence Agocs <bence@harom.studio>
2020-04-03 09:31:27 +0200

    npm installed, sass and watcher tasks added, gitignore created

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/scss/**/*.scss', gulp.series('sass'));
+  console.log('gulp is watching for SCSS changes 👀');
+});
+
+gulp.task('default', gulp.series('sass', 'sass:watch'));
