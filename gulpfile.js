89fb4d9aa509a476d9a521610d760dce6e0bfde2
Bence Agocs <bence@harom.studio>
2020-04-03 10:30:45 +0200

    fonts folder and gulp task added, imagemin added

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const   gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        imagemin = require('gulp-imagemin');
  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./assets/scss/**/*.scss')
+  return gulp.src('./assets/src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/dist/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./assets/src/scss/**/*.scss', gulp.series('sass'));
   console.log('gulp is watching for SCSS changes 👀');
 });
 
-gulp.task('default', gulp.series('sass', 'sass:watch'));
+gulp.task('imagemin', async function () {
+  gulp.src('assets/src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('assets/dist/img'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('assets/src/scss/fonts/')
+    .pipe(gulp.dest('assets/dist/css/fonts/'));
+});
+
+gulp.task('default', gulp.series('imagemin', 'fonts', 'sass', 'sass:watch'));
